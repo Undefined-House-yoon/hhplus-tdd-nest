6a6e4cc7fa20c12c800e17d941b6163d80ced020
asd <pa6712@naver.com>
2024-06-18 22:04:13 +0900

    feat: controller 추가

---
+++ src/point/interfaces/point.interfaces.ts
@@ -0,0 +1,27 @@
+import { PointHistory, TransactionType, UserPoint } from '../point.model';
+
+export interface IUserPointTable {
+  getUserPoint(userId: number): Promise<UserPoint>;
+
+  updateUserPoint(userId: number, amount: number): Promise<UserPoint>;
+}
+
+export interface IPointHistoryTable {
+  getUserHistory(userId: number): Promise<PointHistory[]>;
+
+  addHistory(
+    userId: number,
+    type: TransactionType,
+    amount: number,
+  ): Promise<PointHistory>;
+}
+
+export interface IPointService {
+  getPoint(userId: number): Promise<UserPoint>;
+
+  getHistory(userId: number): Promise<PointHistory[]>;
+
+  chargePoint(userId: number, amount: number): Promise<UserPoint>;
+
+  usePoint(userId: number, amount: number): Promise<UserPoint>;
+}
